09b6d24f0e45cc2db409864670d41abdce879ce8
Matt Phillips <mattcphillips17@gmail.com>
2024-01-12 19:54:03 -0700

    basic outline done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import { Header } from './components/common/Header'
+import { Footer } from './components/common/Footer'
+import HomePage from './components/pages/homePage'
+
+export function App() {
+  return (
+    <div className='App'>
+      <Header />
+      
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+      
+      <Footer />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
