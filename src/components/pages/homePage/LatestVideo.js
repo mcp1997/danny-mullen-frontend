520bd4058f9db8d5de01606139a5fd17a56fcbe7
Matt Phillips <mattcphillips17@gmail.com>
2024-02-27 14:13:17 -0700

    re-enabled youtube API

--- src/components/pages/homePage/LatestVideo.js
+++ src/components/pages/homePage/LatestVideo.js
@@ -5,15 +5,15 @@ import YouTubeVid from 'react-youtube'
 export default function LatestVideo() {
   const [latestVideo, setLatestVideo] = useState('')
 
-  // useEffect(() => {
-  //   axios.get('https://yt.lemnoslife.com/noKey/playlistItems?part=snippet&playlistId=UUnyxus3H0_-F8Q6gQTkg41g&maxResults=1')
-  //   .then(res => {
-  //     setLatestVideo(res.data.items[0].snippet.resourceId.videoId)
-  //   })
-  //   .catch(err => {
-  //     console.error(err)
-  //   })
-  // }, [])
+  useEffect(() => {
+    axios.get('https://yt.lemnoslife.com/noKey/playlistItems?part=snippet&playlistId=UUnyxus3H0_-F8Q6gQTkg41g&maxResults=1')
+    .then(res => {
+      setLatestVideo(res.data.items[0].snippet.resourceId.videoId)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, [])
 
   const opts = {
     height: '432',
